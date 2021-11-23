6af4e3b69d2d8f2170d034d5670fdb7671998469
StanVDL <92679342+StanVDL@users.noreply.github.com>
2021-11-23 14:05:59 +0100

    yesefsef

--- EscapeFromIt/Assets/Scripts/Level/Timer.cs
+++ EscapeFromIt/Assets/Scripts/Level/Timer.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     public float timeValue = 90;
+
+    public Text timeText;
 
     // Update is called once per frame
     void Update()
@@ -18,10 +21,19 @@ public class Timer : MonoBehaviour
         {
             timeValue = 0;
         }
+
+        DisplayTime(timeValue);
     }
 
     void DisplayTime(float timeToDisplay)
     {
+        if (timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
 
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{00}", seconds);
     }
 }
